d54d6ae4167ca0db29cf970dc8618cc9fe9a01bf
Ahmed-Ghareb <ahmed2003ghareb@gmail.com>
2025-07-11 16:39:00 +0300

    initial commit from first-project-branch

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>app/page.js</code>.
+            Ahmed Gharib --------------------<span>app/page.js</span>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
